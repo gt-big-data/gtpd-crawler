e6ccc9c46412ffc9802413495245e90b90d2d153
Sam Marder <sam@marder.co>
2016-11-14 11:32:25 -0500

    Made scripts executable.

--- scripts/install_mongo_ubuntu.sh
+++ scripts/install_mongo_ubuntu.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # Only run this script if you're on Ubuntu.
-echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+# https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
